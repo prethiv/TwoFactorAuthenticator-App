5a07d610330e42c27cdc5850fac05f0ed69a2156
Prethiv <prethiv191@gmail.com>
2021-06-27 17:25:00 +0530

    Able to provide different lan ip's

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button ,Image } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button ,Image ,ToastAndroid ,AsyncStorage } from 'react-native';
 let axios = require('axios');
 import * as LocalAuthentication from 'expo-local-authentication';
 
-function Authorize(){
+function Authorize(text){
   console.log("Inside Authorize block")
-  fetch('http://192.168.1.8:4500/auth', {
+  let url='http://'
+  url+=text
+  url+=':4500'
+  url+='/auth'
+  if(text==""||text===''||text===undefined){
+    url='http://192.168.1.8:4500/auth'
+  }
+  console.log("Logging out url ",url)
+  fetch(url, {
   method: 'POST',
   headers: {
     Accept: 'application/json',
@@ -16,6 +24,13 @@ function Authorize(){
     auth:"AUTH"
   })
   });
+   ToastAndroid.showWithGravityAndOffset(
+        "Authentication Success",
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        25,
+        50
+      );
 }
 
 function SendRejected(){
@@ -30,10 +45,41 @@ function SendRejected(){
     auth:"REJECT"
   })
   });
+  ToastAndroid.showWithGravityAndOffset(
+        "Authentication Failed",
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        25,
+        50
+      );
 }
 
-async function CheckSensor(){
+async function setKeyVal(val){
+    console.log("Inside setkeyvalue pair")
+    try{
+      await AsyncStorage.setItem('ip',val)
+    }
+    catch(err){
+      console.log('Error setting key val pair');
+    }
+}
+
+async function getIp(){
+    console.log("inside Get ip")
+    try{
+     let value=await AsyncStorage.getItem('ip');
+     console.log("Value is ",value)
+     return value;
+    }
+    catch(err){
+      console.log("Error retreiving ip address");
+    }
+}
+
+async function CheckSensor(text){
   console.log("Inside Check Sensor Function")
+  console.log("Ip address entered ",text)
+  setKeyVal(text)
   console.log(LocalAuthentication.hasHardwareAsync())
   console.log(LocalAuthentication.supportedAuthenticationTypesAsync().then(res=>{
     console.log("Response :",res)
@@ -64,10 +110,24 @@ async function CheckSensor(){
     console.log("Response :",res)
     console.log(res.success)
     if(res.success==true){
-      Authorize();
+      Authorize(text);
+      // ToastAndroid.showWithGravityAndOffset(
+      //   "Authorization Success",
+      //   ToastAndroid.LONG,
+      //   ToastAndroid.BOTTOM,
+      //   25,
+      //   50
+      // );
     }
     else{
       SendRejected();
+      // ToastAndroid.showWithGravityAndOffset(
+      //   "Authorization Failed",
+      //   ToastAndroid.LONG,
+      //   ToastAndroid.BOTTOM,
+      //   25,
+      //   50
+      // );
     }
   })
   .catch(err=>{
@@ -87,16 +147,33 @@ async function CheckSensor(){
   
 }
 
+
+
 export default function App() {
+
+  let [text, onChangeText] = React.useState("");
+
+  const textVal=()=>{
+    if(text===""){ return getIp(); }
+    else{ return text; }
+  }
+
   return (
     <View style={styles.container}>
       <Image
         style={styles.tinyLogo}
         source={require('./assets/fingerprint.png')}
       />
+
+    <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Enter manual ip here"
+      />
       
       <Button 
-        onPress={CheckSensor}
+        onPress={()=>CheckSensor(text)}
         title="Approve"
         color="#0ac238"
         borderColor="#000000"
@@ -128,5 +205,10 @@ const styles = StyleSheet.create({
   },
   rejectButton:{
     paddingTop:20
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 0,
   }
 });
